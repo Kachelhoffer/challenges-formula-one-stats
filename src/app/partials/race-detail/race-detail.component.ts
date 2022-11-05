26399f069bbe920d83ca79ee3bc234e1be63a98b
Roelf Kachelhoffer <roelf.kachelhoffer@gmail.com>
2022-11-05 12:15:55 +0200

    Added race item detail for years +Highlighting

--- src/app/partials/race-detail/race-detail.component.ts
+++ src/app/partials/race-detail/race-detail.component.ts
@@ -1,23 +1,41 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FormulaOneAPIService, RaceWinnersForYearType } from 'src/app/services/formula-one-api/formula-one-api.service';
+import { WinnerModelType } from '../card/card.component';
 
 @Component({
   selector: 'app-race-detail',
   templateUrl: './race-detail.component.html',
   styleUrls: ['./race-detail.component.css']
 })
-export class RaceDetailComponent implements OnInit {
+export class RaceDetailComponent implements OnInit  {
   
-  @Input (" yearRaceData:any") yearRaceData:any
+  @Input('yearRaceData') yearRaceData:RaceWinnersForYearType;
+  @Input('worldChampionWinnerModel') worldChampionWinnerModel:WinnerModelType;
 
   @Output("navigateBackEvent") navigateBackEvent = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef, private formulaOneAPIService:FormulaOneAPIService) { }
 
   navigateBackClicked(){
     this.navigateBackEvent.emit("activated");
   }
 
+  getRaces(){
+    if(this.yearRaceData)
+    {
+      console.log("Returning: " + this.yearRaceData.MRData.RaceTable.Races)
+      // return this.yearRaceData.MRData.RaceTable.Races
+    }
+    // else{
+    //   console.log("No Race Data Found")
+    //   return
+    // }
+  }
+
   ngOnInit(): void {
+
+    // this.getDetailForWinnersOfYear(this.yearRaceData.MRData.RaceTable.season)
   }
 
 }
---
+++ src/app/partials/race-item/race-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { RaceModel } from 'src/app/models/RaceModel';
+
+@Component({
+  selector: 'app-race-item',
+  templateUrl: './race-item.component.html',
+  styleUrls: ['./race-item.component.css']
+})
+export class RaceItemComponent implements OnInit {
+
+  @Input('race') race:RaceModel|undefined;
+  @Input('worldChampionDriverID') worldChampionDriverID:string|undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
